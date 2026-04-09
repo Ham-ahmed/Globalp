abcc44c29784fa9561b692a1ec83c6a6428932b7
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2026-04-09 13:19:12 +0200

    Add files via upload

--- GlobalTranslatePro.sh
+++ GlobalTranslatePro.sh
@@ -19,7 +19,7 @@ NC='\033[0m' # No Color
 # Script configuration
 PLUGIN_NAME="GlobalTranslatePro"
 VERSION="5.2"
-GITHUB_RAW="https://raw.githubusercontent.com/Ham-ahmed/GlobalA/refs/heads/main"
+GITHUB_RAW="https://raw.githubusercontent.com/Ham-ahmed/Globalp/refs/heads/main"
 # Try different possible package names
 PACKAGE_NAMES="${PLUGIN_NAME}-${VERSION}.tar.gz ${PLUGIN_NAME}.tar.gz ${PLUGIN_NAME}_${VERSION}.tar.gz plugin.tar.gz"
 TEMP_DIR="/var/volatile/tmp"
